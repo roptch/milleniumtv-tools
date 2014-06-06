3c7689c28130459e2956c7f5568b3220dea6b06e
Romain "dreamstate" Pichot <ropichot@gmail.com>
2014-06-06 21:18:46 +0100

    Changed popup design + default quality management

--- chrome/content-script.js
+++ chrome/content-script.js
@@ -1,17 +1,94 @@
 (function() {
-    window.onload = function() {
-        chrome.storage.sync.get('hideUnblockQuest', function(data) {
-            if (data.hideUnblockQuest === true) {
-                interval = setInterval(function() {
-                    layers = document.getElementsByClassName('UnblockQuest-v2');
-                    if (layers.length > 0) {
-                        clearInterval(interval);
-                        for (i = 0; i < layers.length; ++i) {
-                            layers[i].style.display = 'none';
-                        }
-                    }
-                }, 200);
+    function execWhenClassFound(className, callback) {
+        setTimeout(function() {
+            var elements = document.getElementsByClassName(className);
+
+            if (elements.length > 0) {
+                callback(elements);
+            } else {
+                execWhenClassFound(className, callback);
             }
+        }, 200);
+    }
+
+    function replaceParamInUrl(url, param, value) {
+        var splitUrl = url.split('?');
+        var newUrl = splitUrl[0];
+        var queryString = splitUrl[1];
+
+        if (splitUrl.length >= 2) {
+            var vars = queryString.split('&');
+
+            for (var i = 0; i < vars.length; ++i) {
+                var pair = vars[i].split('=');
+
+                if (i == 0)
+                    newUrl += '?';
+                else
+                    newUrl += '&';
+
+                if (pair[0] == param)
+                    newUrl += pair[0] + '=' + value;
+                else
+                    newUrl += pair[0] + '=' + pair[1];
+            }
+        }
+
+        return newUrl;
+    }
+
+    window.onload = function() {
+        chrome.storage.sync.get({'hideUnblockQuest': true}, function(data) {
+            if (data.hideUnblockQuest === true) {
+                execWhenClassFound('UnblockQuest-v2', function(layers) {
+                    for (i = 0; i < layers.length; ++i) {
+                        layers[i].style.display = 'none';
+                    }
+                });
+            }
+        });
+
+        chrome.storage.sync.get({'quality': 720}, function(data) {
+            execWhenClassFound('available', function(elems) {
+                var qualityList = ['1080', '720', '480', '380', '240'];
+                var availableQualityList = [];
+                var activeQuality;
+                var selectedQuality;
+
+                // Getting all available quality labels in variable "availableQualityList"
+                for (var i = 0; i < elems.length; ++i) {
+                    var classList = elems[i].className.split(' ');
+                    var quality = classList[1].split('-')[2];
+
+                    // Saving the current quality in "activeQuality"
+                    if (classList.indexOf('active') != -1)
+                        activeQuality = quality;
+
+                    availableQualityList.push(quality);
+                }
+
+                var mergedQualityList = [];
+                for (var i = 0; i < qualityList.length; ++i) {
+                    if (availableQualityList.indexOf(qualityList[i]) != -1)
+                        mergedQualityList.push(qualityList[i]);
+                    else
+                        mergedQualityList.push(null);
+                }
+
+                var i = qualityList.indexOf(data.quality);
+                while (i < mergedQualityList.length && mergedQualityList[i] == null)
+                    ++i;
+
+                if (i == mergedQualityList.length)
+                    selectedQuality = availableQualityList[availableQualityList.length - 1];
+                else
+                    selectedQuality = mergedQualityList[i];
+
+                if (selectedQuality == activeQuality)
+                    return ;
+
+                window.location.href = replaceParamInUrl(document.URL, 'quality', selectedQuality);
+            });
         });
     }
 })();
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,13 +1,30 @@
 (function() {
-    window.onload = function() {
-        unblockQuestCheckbox = document.getElementById('unblockquest');
+    function manageUnblockQuest() {
+        var unblockQuestCheckbox = document.getElementById('unblockquest');
 
-        chrome.storage.sync.get('hideUnblockQuest', function(data) {
+        chrome.storage.sync.get({'hideUnblockQuest': true}, function(data) {
             unblockQuestCheckbox.checked = data.hideUnblockQuest;
         });
 
         unblockQuestCheckbox.onchange = function() {
             chrome.storage.sync.set({'hideUnblockQuest': unblockQuestCheckbox.checked});
         }
+    }
+
+    function manageQuality() {
+        var qualitySelect = document.getElementById('quality');
+
+        chrome.storage.sync.get({'quality': 720}, function(data) {
+            qualitySelect.value = data.quality;
+        });
+
+        qualitySelect.onchange = function() {
+            chrome.storage.sync.set({'quality': qualitySelect.value});
+        }
+    }
+
+    window.onload = function() {
+        manageUnblockQuest();
+        manageQuality();
     }
 })();
